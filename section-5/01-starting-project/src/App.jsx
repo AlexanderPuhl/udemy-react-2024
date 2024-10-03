0c3380064a3516e91ec7c9b1acb42274f76f5c41
Alexander Puhl <alexpuhldeveloper@gmail.com>
2024-10-02 22:25:34 -0600

    Half way done with section 12

--- section-5/01-starting-project/src/App.jsx
+++ section-5/01-starting-project/src/App.jsx
@@ -1,11 +1,34 @@
+import { useState } from 'react';
 import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
 
 function App() {
+	const [userInput, setUserInput] = useState({
+		initialInvestment: 10000,
+		annualInvestment: 1200,
+		expectedReturn: 6,
+		duration: 10,
+	});
+
+	function handleChange(inputIdentifier, newValue) {
+		console.log(typeof newValue);
+		setUserInput((prevUserInput) => {
+			return {
+				...prevUserInput,
+				[inputIdentifier]: Number(newValue),
+			};
+		});
+	}
+
 	return (
 		<>
 			<Header />
-			<UserInput />
+			<UserInput
+				userInput={userInput}
+				setUserInput={setUserInput}
+				onChange={handleChange}
+				onChange
+			/>
 		</>
 	);
 }
--- section-5/01-starting-project/src/components/UserInput.jsx
+++ section-5/01-starting-project/src/components/UserInput.jsx
@@ -1,42 +1,26 @@
-import { useState } from 'react';
 
-export default function UserInput() {
-	const [userInput, setUserInput] = useState({
-		initialInvestment: 10000,
-		annualInvestment: 1200,
-		expectedReturn: 6,
-		duration: 10,
-	});
-
-	function handleChange(inputIdentifier, newValue) {
-		setUserInput((prevUserInput) => {
-			return {
-				...prevUserInput,
-				[inputIdentifier]: newValue,
-			};
-		});
-	}
+export default function UserInput({onChange}) {
 
 	return (
 		<section id="user-input">
 			<div className="input-group">
 				<p>
 					<label htmlFor="">Initial Investment</label>
-					<input type="number" required />
+					<input type="number" required value={onChange.initialInvestment} onChange={(event) => onChanges('initialInvestment', event.target.value)} />
 				</p>
 				<p>
 					<label htmlFor="">Annual Investment</label>
-					<input type="number" required />
+					<input type="number" required value={onChange.annualInvestment} onChange={(event) => onChanges('annualInvestment', event.target.value)} />
 				</p>
 			</div>
 			<div className="input-group">
 				<p>
 					<label htmlFor="">Expected Return</label>
-					<input type="number" required />
+					<input type="number" required value={onChange.expectedReturn} onChange={(event) => onChanges('expectedReturn', event.target.value)} />
 				</p>
 				<p>
 					<label htmlFor="">Duration</label>
-					<input type="number" required />
+					<input type="number" required value={onChange.duration} onChange={(event) => onChanges('duration', event.target.value)} />
 				</p>
 			</div>
 		</section>
