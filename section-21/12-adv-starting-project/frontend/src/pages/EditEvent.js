2de6750ba2a0ef68dd32ba1f739afdbf152161e1
Alexander Puhl <alexpuhldeveloper@gmail.com>
2024-10-08 11:12:04 -0600

    save point

--- section-21/12-adv-starting-project/frontend/src/pages/EditEvent.js
+++ section-21/12-adv-starting-project/frontend/src/pages/EditEvent.js
@@ -1,7 +1,12 @@
+import { useRouteLoaderData } from 'react-router-dom';
+import EventForm from '../components/EventForm';
+
 export default function EditEventPage() {
+	const data = useRouteLoaderData('event-detail');
+
 	return (
 		<>
-			<h1>Edit Event Page</h1>
+			<EventForm event={data.event} />
 		</>
 	);
 }
