d63c4950ce3d99302b922da37c38a84abf11dbcf
Alex Puhl <alex.puhl@mityinc.com>
2024-10-09 12:02:41 -0600

    Finished video 202

--- section-12/01-starting-project/src/components/Quiz.jsx
+++ section-12/01-starting-project/src/components/Quiz.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 import QUESTIONS from '../questions';
+import QuestionTimer from './QuestionTimer';
+import Answers from './Answers';
+import quizIsCompleteImg from '../assets/quiz-complete.png';
+import Question from './Question';
 
 export default function Quiz() {
 	const [userAnswers, setUserAnswers] = useState([]);
-	const activeQuestionIndex = userAnswers.length;
 
-	function handleSelectAnswer(selectedAnswer) {
+	const activeQuestionIndex = userAnswers.length;
+	const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+	const handleSelectAnswer = useCallback(function handleSelectAnswer(
+		selectedAnswer
+	) {
 		setUserAnswers((prevUserAnswers) => {
 			return [...prevUserAnswers, selectedAnswer];
 		});
+	},
+	[]);
+
+	const handleSkipAnswer = useCallback(
+		() => handleSelectAnswer(null),
+		[handleSelectAnswer]
+	);
+
+	if (quizIsComplete) {
+		return (
+			<div id="summary">
+				<img src={quizIsCompleteImg} alt="Trophy Icon" />
+				<h2>Quis is completed.</h2>
+			</div>
+		);
 	}
 
 	return (
 		<div id="quiz">
-			<div id="question">
-				<h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-				<ul id="answers">
-					{QUESTIONS[activeQuestionIndex].answers.map((answer) => {
-						return (
-							<li key={answer} className="answer">
-								<button onClick={() => handleSelectAnswer(answer)}>
-									{answer}
-								</button>
-							</li>
-						);
-					})}
-				</ul>
-			</div>
+			<Question
+				key={activeQuestionIndex}
+				index={activeQuestionIndex}
+				onSelectAnswer={handleSelectAnswer}
+				onSkipAnswer={handleSkipAnswer}
+			/>
 		</div>
 	);
 }
